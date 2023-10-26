c7256d989c42616fde1f4fa582f416b751064085
aaa2002 <alexand2002@gmail.com>
2023-10-26 23:03:54 +0300

    Created main page, added random text for testing, created navbar

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -10,7 +10,7 @@ const myCustomLightTheme = {
   colors: {
     background: "#FFFFFF",
     surface: "#FFFFFF",
-    primary: "#6200EE",
+    primary: "#006837",
     "primary-darken-1": "#3700B3",
     secondary: "#03DAC6",
     "secondary-darken-1": "#018786",
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,52 @@
 <template>
-  <div>
-    <img alt="photo" src="../assets/images/angie.png" />
-  </div>
+  <v-img alt="photo" src="../assets/images/angie.png">
+    <div class="image-text">
+      <v-img
+        src="../assets/images/erasmus.png"
+        alt="Erasmus"
+        max-height="50px"
+      /><span>2023-1-RO01-KA220-HED-000158031</span>
+    </div>
+  </v-img>
+  <v-container>
+    <h1>blah</h1>
+    <p>Lorem ipsum dolor sit amet</p>
+    <h1>Content</h1>
+    <p>Lorem ipsum dolor sit amet</p>
+    <h1>Continut</h1>
+    <p>Lorem ipsum dolor sit amet</p>
+    <h1>blah</h1>
+    <p>Lorem ipsum dolor sit amet</p>
+    <h1>Content</h1>
+    <p>Lorem ipsum dolor sit amet</p>
+    <h1>Continut</h1>
+    <p>Lorem ipsum dolor sit amet</p>
+    <h1>blah</h1>
+    <p>Lorem ipsum dolor sit amet</p>
+    <h1>Content</h1>
+    <p>Lorem ipsum dolor sit amet</p>
+    <h1>Continut</h1>
+    <p>Lorem ipsum dolor sit amet</p>
+    <h1>blah</h1>
+    <p>Lorem ipsum dolor sit amet</p>
+    <h1>Content</h1>
+    <p>Lorem ipsum dolor sit amet</p>
+    <h1>Continut</h1>
+    <p>Lorem ipsum dolor sit amet</p>
+  </v-container>
 </template>
 
-<script setup></script>
+<script lang="ts" setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.image-text {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+  color: white;
+  padding: 5px;
+  font-size: 16px;
+  border-radius: 0 55% 0 0;
+}
+</style>
