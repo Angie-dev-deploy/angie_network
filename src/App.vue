dd7aa0ac0b4fb5f2b349161f69384ed68dd88d4d
PavelDrg <pavel.dragan2@gmail.com>
2023-10-27 23:26:21 +0300

    Added photo carroussel skeleton.

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
     <NavigationBar />
 
     <router-view />
+    <PhotoCarroussel />
   </v-app>
 </template>
 
 <script lang="ts" setup>
 import NavigationBar from "./components/NavigationBar.vue";
+import PhotoCarroussel from "./components/PhotoCarroussel.vue";
 </script>
