a938d6e2cf3eadae5fa40adfab704d10cb9413f2
PavelDrg <pavel.dragan2@gmail.com>
2023-10-29 23:31:23 +0200

    Implemented footer (not yet reactive)

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,4 +1,33 @@
 <template>
+  <div class="footer">
+    <div class="footer__container">
+      <div class="footer__container-element">
+        <h3>Contact Us</h3>
+        <p>E-mail:</p>
+        <p>Phone:</p>
+      </div>
+      <div class="footer__container-element">
+        <h3>Follow Us</h3>
+        <div class="icons">
+          <v-icon>mdi-facebook</v-icon>
+          <v-icon>mdi-instagram</v-icon>
+          <v-icon>mdi-linkedin</v-icon>
+        </div>
+      </div>
+      <div class="footer__container-element">
+        <img :src="erasmus_logo" alt="erasmus_logo" />
+        <p>2023-1-RO01-KA220-HED-000158031</p>
+      </div>
+    </div>
+    <div class="footer__container">
+      <div class="footer__container-element">
+        <p>© 2023 All rights reserved.</p>
+      </div>
+      <div class="footer__container-element">
+        <img :src="angie_logo" alt="angie_logo" class="logo" />
+      </div>
+    </div>
+  </div>
   <div class="wave">
     <img class="ivyLeft" :src="ivyLeft" alt="" />
     <img class="ivyRight" :src="ivyRight" alt="" />
@@ -18,25 +47,113 @@
   </div>
 </template>
 
-<script setup>
+<script lang="ts" setup>
 import ivyLeft from "@/assets/images/footer/ivy_left.png";
 import ivyRight from "@/assets/images/footer/ivy_right.png";
+import erasmus_logo from "@/assets/images/footer/erasmus_logo.png";
+import angie_logo from "@/assets/images/footer/angie_logo.png";
 </script>
 
 <style lang="scss" scoped>
 .wave {
   background-color: #e2dcde;
-  padding-top: 200px;
+  padding-top: 300px;
+  z-index: 0;
 }
 
 .ivyLeft {
   position: absolute;
   bottom: 15rem;
+  z-index: 0;
 }
 
 .ivyRight {
   position: absolute;
   bottom: 10rem;
   right: 0;
+  z-index: 0;
+}
+
+.footer {
+  position: absolute;
+  bottom: 0;
+  z-index: 2;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+.footer__container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 2rem 0;
+  z-index: 1;
+}
+
+.footer__container-element {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .logo {
+    width: 100%;
+    max-width: 15vw;
+  }
+}
+
+.footer__container-element h3 {
+  color: #e2dcde;
+  font-size: 45px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin-bottom: 1rem;
+}
+
+.footer__container-element p {
+  color: #e2dcde;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+
+.footer__container-element img {
+  margin-bottom: 1rem;
+}
+.icons {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  font-size: 25px;
+  color: #e2dcde;
+}
+
+@media screen and (max-width: 768px) {
+  .footer {
+    flex-direction: column; /* Stack elements vertically on smaller screens */
+  }
+
+  .footer__container {
+    flex-direction: column; /* Stack elements vertically on smaller screens */
+  }
+
+  .footer__container-element h3 {
+    font-size: 28px; /* Adjust the font size for smaller screens */
+  }
+
+  .footer__container-element p {
+    font-size: 16px; /* Adjust the font size for smaller screens */
+  }
+
+  .footer__container-element img {
+    max-width: 80%; /* Adjust image size for smaller screens */
+  }
+
+  .icons {
+    font-size: 20px; /* Adjust the icon size for smaller screens */
+  }
 }
 </style>
